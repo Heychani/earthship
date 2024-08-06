a7aa15d5eb584aa6cac19672ac8a1daedf325335
Heychani <chanielomb@gmail.com>
2024-08-06 08:30:05 +0200

    Route and Error Handlers (user, product and update)

--- src/server.ts
+++ src/server.ts
@@ -3,24 +3,29 @@ import router from './router'
 import morgan from 'morgan'
 import cors from 'cors'
 import { protect } from './modules/auth'
-import { createNewUser, signIn } from './handlers/user'
+import { createNewUser, signin } from './handlers/user'
 
-const app = express() //make the api
+const app = express()
 
 app.use(cors())
-app.use(morgan('dev')) //middleware at top, use it for the entire app due to it being in the main app
-app.use(express.json()) // this one allows a client to send us json, without this we would have to manually do it
-app.use(express.urlencoded({extended: true})) // allows a client to add a string or enquire a string, if you don't do this it won't allow a query string
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-app.get('/', (req, res) => {
-    console.log('hello from express');
-    res.status(200)
-    res.json({message: 'hello'})
+app.get('/', (req, res, next) => {
+  setTimeout(() => {
+    next(new Error('hello'))
+  },1)
 })
 
-app.post("/user", createNewUser);
-app.post("/signIn", signIn);
-
 app.use('/api', protect, router)
+
+app.post('/user', createNewUser)
+app.post('/signin', signin)
+
+app.use((err, req, res, next) => {
+  console.log(err)
+  res.json({message: `had an error: ${err.message}`})
+})
 
 export default app
